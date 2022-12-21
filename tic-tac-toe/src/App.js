f838ae2f917bb7d7ed9e987b3826a3147ea839d4
DarkHouse123 <ivokarapeev@gmail.com>
2022-12-21 09:36:28 +0200

    Adding the Game Logic

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,14 +1,32 @@
-import './App.css';
+import { useState } from 'react'
 
+import './App.css';
 import Board from './components/Board';
 
 function App() {
 
-    const board = ['X','X','X','X','X','X','X','X','X']
+    const [board,setBoard] = useState(Array(9).fill(null));
+    const [xPlaying,setXPlaying] = useState(true);
+
+    const boxClick = (boxIndex) => {
+        const updatedBoard = board.map((value,indx) => {
+            if (indx === boxIndex) {
+                return xPlaying === true ? 'X' : 'O';
+            }else{
+                return value
+            }
+        });
+
+
+        setBoard(updatedBoard);
+
+        setXPlaying(!xPlaying);
+    }
+
 
     return (
         <div className="App">
-            <Board board={board} onClick={null}/>
+            <Board board={board} onClick={boxClick}/>
         </div>
     );
 }
